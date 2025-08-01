96a86b9c8e7271025bec6692d0ddca3b40f4c3e4
RaivisVolkovs <raivis.volkovs14@gmail.com>
2025-08-01 06:21:58 -0400

    add edit, toggle and print methods to see the todo list

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("Inside main in todo app")
+	todos := Todos{}
+	todos.add("Learn Go")
+	todos.add("Build a Todo app")
+	todos.toggle(0)
+	todos.print()
 }
--- todo.go
+++ todo.go
@@ -3,7 +3,11 @@ package main
 import (
 	"errors"
 	"fmt"
+	"os"
+	"strconv"
 	"time"
+
+	"github.com/aquasecurity/table"
 )
 
 type Todo struct {
@@ -34,4 +38,71 @@ func (todos *Todos) validateIndex(index int) error {
 		return err
 	}
 	return nil
+}
+
+//delete method
+
+func (todos *Todos) delete(index int) error {
+	t := *todos
+
+	if err := t.validateIndex(index); err != nil {
+		return err
+	}
+
+	*todos = append(t[:index], t[index+1:]...)
+	return nil
+}
+
+// toggle method to mark a todo as completed or not
+func (todos *Todos) toggle(index int) error {
+	t := (*todos)
+	if err := t.validateIndex(index); err != nil {
+		return err
+	}
+
+	isCompleted := t[index].Completed
+
+	if !isCompleted {
+		completionTime := time.Now()
+		t[index].CompletedAt = &completionTime
+	}
+
+	t[index].Completed = !isCompleted
+
+	return nil
+}
+
+func (todos *Todos) edit(index int, title string) error {
+	t := (*todos)
+	if err := t.validateIndex(index); err != nil {
+		return err
+	}
+
+	t[index].Title = title
+
+	return nil
+}
+
+func (todos *Todos) print() {
+	table := table.New(os.Stdout)
+	table.SetRowLines(false)
+	table.SetHeaders("#", "Title", "Completed", "Created At", "Completed At")
+	for index, t := range *todos {
+		completed := "FALSE"
+		completedAt := "N/A"
+
+		if t.Completed {
+			completed = "TRUE"
+			if t.CompletedAt != nil {
+				completedAt = t.CompletedAt.Format(time.RFC1123)
+
+			}
+
+		}
+
+		table.AddRow(strconv.Itoa(index), t.Title, completed, t.CreatedAt.Format(time.RFC1123), completedAt)
+
+	}
+	table.Render()
+
 }
