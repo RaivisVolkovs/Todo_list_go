47a9ef229c2d388e492789c0f587883e771fd09e
RaivisVolkovs <raivis.volkovs14@gmail.com>
2025-08-01 05:31:49 -0400

    setup Todo structure and add initial methods (add, validateIndex, delete)

---
+++ todo.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+type Todo struct {
+	Title       string
+	Completed   bool
+	CreatedAt   time.Time
+	CompletedAt *time.Time //can be null "*"
+}
+
+type Todos []Todo
+
+func (todos *Todos) add(title string) {
+	todo := Todo{
+		Title:       title,
+		Completed:   false,
+		CompletedAt: nil,
+		CreatedAt:   time.Now(),
+	}
+
+	*todos = append(*todos, todo)
+}
+
+// index validation
+func (todos *Todos) validateIndex(index int) error {
+	if index < 0 || index >= len(*todos) {
+		err := errors.New("Invalid index")
+		fmt.Println(err)
+		return err
+	}
+	return nil
+}
